241afb69521a87e019e461fd17e5feca217581b3
nopara73 <adam.ficsor73@gmail.com>
2018-01-13 15:47:06 -0400

    initial commit

--- UTF8Problems/UnitTest1.cs
+++ UTF8Problems/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using Xunit;
 
 namespace UTF8Problems
@@ -6,9 +7,36 @@ namespace UTF8Problems
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanEncode()
         {
+            var character = "é";
+            var encoded = Encoding.UTF8.GetBytes(character);
 
+            var bytes = new byte[] { 195, 169 };
+
+            Assert.Equal(bytes, encoded);
+        }
+
+        [Fact]
+        public void CanDecode()
+        {
+            var character = "é";
+
+            var bytes = new byte[] { 195, 169 };
+            var decoded = Encoding.UTF8.GetString(bytes);
+            
+            Assert.Equal(character, decoded);
+        }
+
+        [Fact]
+        public void CanEncodeDecode()
+        {
+            var character = "é";
+            var encoded = Encoding.UTF8.GetBytes(character);
+
+            var decoded = Encoding.UTF8.GetString(encoded);
+
+            Assert.Equal(character, decoded);
         }
     }
 }
